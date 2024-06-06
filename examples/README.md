73f18113baa6dd5f3ead54a2b9adf1893b5e89ce
Sebastian Guenther <sebastian.guenther@ifes.uni-hannover.de>
2024-06-06 11:24:51 +0200

    dev *added doc and material for examples folder
    On branch develop
    Changes to be committed:
          modified:   examples/.gitignore
          new file:   examples/README.md
          modified:   examples/demonstrate_simulation.ipynb
          modified:   examples/ems_comparison.py
          new file:   examples/estss64_ems_simulations.pdf

---
+++ examples/README.md
@@ -0,0 +1,15 @@
+# examples/ folder description
+
+- demonstrate_simulation.ipynb
+  - Shows the basic functionality of the hess-ems-sim toolbox
+- ems_comparison.py
+  - File that contains functions to generate the time series figures of the plot
+    as well as the additional material mentioned there. It simulates and plots
+    all other time series of the estss_n64 data set
+  - Associated files are:
+    - estss64.pkl
+      - contains the time series data of the estss64 data set
+    - estss64_hybrid_results.mat
+      - contains the results of the hybrid toolbox for the according time series
+    - estss64_ems_simulations.pdf
+      - Contains the plots of the ems simulations for all estss64 tiime series
--- examples/ems_comparison.py
+++ examples/ems_comparison.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 from pathlib import Path
+from time import time
 
 import numpy as np
 import pandas as pd
@@ -9,13 +11,15 @@ import scipy.io as sio
 from scipy import fft
 from hessemssim import (SimSetup, Storage, InputData, FilterController,
                         DeadzoneController, FuzzyController, MPCController,
-                        PassthroughFallbackController)
+                        PassthroughFallbackController, ProportionalController)
 
 
 ESTSS_PATH = Path(__file__).parent / 'estss64.pkl'
 
 
 def get_estss_data(path=ESTSS_PATH, which='all'):
+    """Read pickle file that contains the estss_n64 data. Depending on the
+    value of `which`, the function returns different data."""
     data = pd.read_pickle(path)
     neg = data.iloc[:, :32]
     posneg = data.iloc[:, 32:]
@@ -46,11 +50,15 @@ def mat_export(
         datasets=get_estss_data(which='as_tuple'),  # noqa
         varnames=('all', 'neg', 'posneg', 'showcase')
 ):
+    """Exports the passed data as a .mat file to be able to process it with the
+    Matlab `hybrid` toolbox (github.com/s-guenther/hybrid)"""
     data_dict = {k: v.to_numpy() for k, v in zip(varnames, datasets)}
     return sio.savemat(path, data_dict)
 
 
 def mat_import(path='examples/estss64_hybrid_results.mat'):
+    """Reads the generated data from the Matlab `hybrid` toolbox
+    (github.com/s-guenther/hybrid) and returns as a data dict."""
     varnames = ('res_all', 'res_neg', 'res_posneg', 'res_showcase')
     header = [
         'hyb_pot', 'rel_pot', 'hyb_skew', 'crosspoint_within', 'overdim',
@@ -74,38 +82,57 @@ def mat_import(path='examples/estss64_hybrid_results.mat'):
     return data_dict
 
 
+# Load all data that is saved to the disc into the following two variables
 TS = get_estss_data(which='as_dict')
 HYB = mat_import()
 
 
 def generate_estss64_plots():
-    ts_ids = TS['all'].columns
+    """Simulates all EMS for all estss time series, plots them and saves to
+    disc."""
+    print('Start')
+    ts_ids = TS['all'].columns[:62]
     sim_args = [_sim_args_general(ts_id) for ts_id in ts_ids]
-    list_of_setup_lists = [sim_mult_ems_for_one_ts(*args) for args in sim_args]
+    list_of_setup_lists = []
+    print('##\n## Simulate\n##')
+    for ii, args in enumerate(sim_args):
+        print(f'Simulate #{ii+1}/{len(sim_args)}', end='')
+        s = time()
+        res = sim_mult_ems_for_one_ts(*args)
+        list_of_setup_lists.append(res)
+        print(f' ... elapsed time:{time() - s}')
     titles = [f'estss{ts_id}' for ts_id in ts_ids]
     kwargs = dict(figsize=(20, 11.25))
-    subfiglbls = 'Filter Deadzone Fuzzy MPC Neural'.split()
+    subfiglbls = ['Filter-based EMS',
+                  'Deadzone-based EMS',
+                  'Fuzzy-logic-based EMS',
+                  'Model-predictive-control-based EMS']
     plt.rcParams['path.simplify'] = False
     plt.rcParams['path.simplify_threshold'] = 1.0
-    fig_axs_tuples = [plot_comparison(slist, subfiglbls, title, **kwargs)
-                      for slist, title
-                      in zip(list_of_setup_lists, titles)]
-    # for (fig, ax), title in zip(fig_axs_tuples, titles):
-    #     fig.savefig(f'{title}.png', format='png', dpi=300)
+    fig_axs_tuples = []
+    print('##\n## Print\n##')
+    for ii, (slist, title) in enumerate(zip(list_of_setup_lists, titles)):
+        print(f'Print #{ii+1}/{len(list_of_setup_lists)}')
+        fig, ax = plot_comparison(slist, subfiglbls, title, axarrows=False, **kwargs)
+        fig.savefig(f'{title}.png', format='png', dpi=300)
+        fig.savefig(f'{title}.jpg', format='jpg', dpi=300)
+        fig.savefig(f'{title}.svg', format='svg')
+        fig.savefig(f'{title}.pdf', format='pdf')
+        fig_axs_tuples.append((fig, ax))
+    print('End')
     return list_of_setup_lists, fig_axs_tuples
 
 
 def generate_paper_plots():
-    """Generates data for two time series for each of the 5 EMS and calls
+    """Generates data for two time series for each of the 4 EMS and calls
     `plot_comparison()`with this data."""
 
     sim_args = [_sim_args_for_showcase_15(), _sim_args_for_showcase_09()]
     list_of_setup_lists = [sim_mult_ems_for_one_ts(*args) for args in sim_args]
-    # subfiglbls = ['Filter-based EMS',
-    #               'Deadzone-based EMS',
-    #               'Fuzzy-logic-based EMS',
-    #               'Model-predictive-control-based EMS']
-    subfiglbls = ['', '', '', '']
+    subfiglbls = ['Filter-based EMS',
+                  'Deadzone-based EMS',
+                  'Fuzzy-logic-based EMS',
+                  'Model-predictive-control-based EMS']
     kwargs = dict(figsize=(8.9/2.54, 11.5/2.54))
     fig_axs_tuples = [plot_comparison(slist, subfiglbls, **kwargs)
                       for slist
@@ -113,30 +140,10 @@ def generate_paper_plots():
     return list_of_setup_lists, fig_axs_tuples
 
 
-def _sim_args_for_showcase_15():
-    ts, stor_para, con_para = _sim_args_general(15, 'showcase')
-
-    con_para[0]['finit'] = -0.35
-    con_para[0]['fcut'] = 0.8
-
-    con_para[1]['gain'] = 0.2
-
-    return ts, stor_para, con_para
-
-
-def _sim_args_for_showcase_09():
-    ts, stor_para, con_para = _sim_args_general(1_000_009, 'showcase')
-
-    con_para[0]['fcut'] = 3.5
-    con_para[0]['finit'] = -0.0
-    con_para[0]['gain'] = 0
-
-    con_para[1]['gain'] = 0.1
-
-    return ts, stor_para, con_para
-
-
 def _sim_args_general(ts_id, which='all', overdim=1.1):
+    """Function that generates the needed simulation arguments for a specific
+    time series id for all ems. Calculates relevant storage parameters and ems
+    parameters. Returns two lists of dicts."""
     ts = TS[which][ts_id].to_numpy()
     res = HYB[which].loc[ts_id, :]
 
@@ -197,7 +204,36 @@ def _sim_args_general(ts_id, which='all', overdim=1.1):
     return ts, stor_para, con_para
 
 
+def _sim_args_for_showcase_15():
+    """Manual overwrite of some parametes of the general sim_args data dicts
+    that are specifically tuned for the showcase_15 example."""
+    ts, stor_para, con_para = _sim_args_general(15, 'showcase')
+
+    con_para[0]['finit'] = -0.35
+    con_para[0]['fcut'] = 0.8
+
+    con_para[1]['gain'] = 0.2
+
+    return ts, stor_para, con_para
+
+
+def _sim_args_for_showcase_09():
+    """Manual overwrite of some parametes of the general sim_args data dicts
+    that are specifically tuned for the showcase_09 example."""
+    ts, stor_para, con_para = _sim_args_general(1_000_009, 'showcase')
+
+    con_para[0]['fcut'] = 3.5
+    con_para[0]['finit'] = -0.0
+    con_para[0]['gain'] = 0
+
+    con_para[1]['gain'] = 0.1
+
+    return ts, stor_para, con_para
+
+
 def _estimate_filter_cutoff(ts, ratio, adjust=3):
+    """Try to determine a well suited cutoff frequency for the filter-based EMS
+    depending on the power spectrum of the passed time series."""
     f_ts = fft.fft(ts)[1:int(len(ts)/2)]
     power_spec = np.abs(f_ts)  # removed square from official power spectrum
     cum_power = np.cumsum(power_spec)
@@ -208,6 +244,8 @@ def _estimate_filter_cutoff(ts, ratio, adjust=3):
 
 
 def sim_mult_ems_for_one_ts(ts, stor_para=None, con_para=None):
+    """Simulate all available EMS for a specific time series. Use the stor_para
+    and conpara list of dicts to parameterize the simulation."""
     if stor_para is None:
         stor_para = (
             # base
@@ -250,7 +288,7 @@ def sim_mult_ems_for_one_ts(ts, stor_para=None, con_para=None):
 
 
 def plot_comparison(list_of_sim_setups, subfiglabels=None, title=None,
-                    **kwargs):
+                    axarrows=True, **kwargs):
     """Plot function that recieves a list of SimSetup objects. Generates a
     plot with a subfigure for each SimSetup, calls the .plot() method of the
     SimSetup object to plot the power time series into each axes.
@@ -271,17 +309,17 @@ def plot_comparison(list_of_sim_setups, subfiglabels=None, title=None,
 
     # Generate figure and axes objects
     fig, axs = plt.subplots(nplots, 1, sharex='all', **kwargs,
-                            gridspec_kw=dict(top=1, bottom=0.065,
+                            gridspec_kw=dict(top=0.965, bottom=0.065,
                                              left=0.047, right=1,
-                                             hspace=0.10))
+                                             hspace=0.25))
     if title is not None:
         fig.suptitle(title, ha='left', va='top', x=0, y=1)
 
     for ax, setup, label in zip(axs, list_of_sim_setups, labels):
         setup.plot(axs=ax, make_legend=False)
         # Add
-        txt = ax.text(0.01, 1, label,
-                      transform=ax.transAxes, va='top', ha='left')
+        txt = ax.text(-0.05, 1.01, label,
+                      transform=ax.transAxes, va='bottom', ha='left')
         txt.set_path_effects(
             [PathEffects.withStroke(linewidth=3, foreground='w')]
         )
@@ -292,18 +330,32 @@ def plot_comparison(list_of_sim_setups, subfiglabels=None, title=None,
         ax.grid(0)
         ax.set_xlabel('')
         ax.set_xlim([0, 1])
-        ax.set_ylim([1.05, -1.05])
+        ax.set_ylim([1.0, -1.0])
         ax.set_ylabel('Power $p$', labelpad=-1)
         ax.spines['top'].set_visible(False)     # Remove the top spine
         ax.spines['right'].set_visible(False)   # Remove the right spine
         ax.spines['bottom'].set_visible(False)  # Remove the bottom spine
         ax.spines['left'].set_visible(False)    # Remove the left spine
         ax.plot([0, 0], [-1.1, 1.1], 'k', linewidth=1)
+        # arrows
+        if axarrows:
+            xend, y0, x0, yend, = 1, 0, 0, -1
+            lenx, leny, bx, by = 0.04, -0.3, 0.1, 0.012
+            t1 = plt.Polygon([(xend, y0),
+                              (xend - lenx, y0 - bx),
+                              (xend - lenx, y0 + bx)],
+                             color='k', ec=None)
+            t2 = plt.Polygon([(x0, yend),
+                              (x0 - by, yend - leny),
+                              (x0 + by, yend - leny)],
+                             color='k', ec=None, clip_on=False)
+            ax.add_patch(t1)
+            ax.add_patch(t2)
 
     ax.set_xlabel('Time $t$', labelpad=-10)  # noqa
     ax.legend('Input Base Peak Mismatch'.split(),
               ncol=4, columnspacing=1.2, fontsize='small', loc='lower center',
-              bbox_to_anchor=(0.5, -0.33),
+              bbox_to_anchor=(0.5, -0.38),
               bbox_transform=ax.transAxes)
     # lines = ax.get_lines()  # noqa
     # fig.legend(lines,
@@ -312,8 +364,3 @@ def plot_comparison(list_of_sim_setups, subfiglabels=None, title=None,
     # fig.tight_layout()
     # fig.subplots_adjust(bottom=0.1)
     return fig, ax
-
-
-if __name__ == '__main__':
-    SETUPS, FIGAXS = generate_paper_plots()
-    dummybreakpoint = True
